e8a7817dd5260c93570b5c6eef43c2acf6227859
jinfenglin <ljfnwpusife@gmail.com>
2015-10-08 19:46:03 -0400

    add mallet

---
+++ mallet-2.0.8RC2/make_train.py
@@ -0,0 +1,21 @@
+import subprocess
+if __name__=="__main__":
+    with open("./sample-data/numeric/alltest.csv") as input_file, open("./test/alltest.meta",'w') as  output_file:
+        i=0
+        for line in input_file:
+            label,f1,f2=line.split(',')
+            f2=f2[:-2]
+            #output_file.write(label+" "+f1+" "+f2+"\n")
+            #output_file.write(label+" "+f1+":"+f1+" "+f2+":"+f2+"\n")
+            output_file.write(label+" "+f1+":1"+" "+f2+":1"+"\n")
+            #output_file.write(str(i)+","+label+","+"f1="+f1+","+"f2="+f2+"\n")
+            i+=1
+###### Import Data######
+    #subprocess.call(["./bin/csv2vectors","--input","./test/alltest.meta","--output","./test/alltest.mallet"])
+    subprocess.call(["./bin/mallet","import-svmlight","--input","./test/alltest.meta","--output","./test/alltest.mallet"])
+###### Check Data######
+    subprocess.call(["./bin/vectors2info","--input","./test/alltest.mallet","--print-matrix"]);
+###### Classify ######
+    #subprocess.call(["./bin/mallet","train-classifier","--input","./test/alltest.mallet","--trainer","MaxEnt","--training-portion","0.9"]);
+
+
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-<head> Readme </head>
+#Readme
+##Files:
+- __MetaData:__
+	Extracted from raw data for generating feature vectors.
+- __Vectors:__
+	Directly usable data for mallet classification.
+- __Src:__
+	Python scripts for all purpose processing.
+- __mallet:__
+	Mallect package with license.
+- __XML Data:__
+	Raw data file
+
+
