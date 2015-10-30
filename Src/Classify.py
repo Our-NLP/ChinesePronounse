5920d892d5200511b7e5288220c7ea2ab65cb5da
jinfenglin <ljfnwpusife@gmail.com>
2015-10-30 02:38:41 -0400

    fit windows

--- Src/Classify.py
+++ Src/Classify.py
@@ -1,8 +1,11 @@
 import subprocess
+import os
 if __name__=="__main__":
-    mallet_bin="../mallet-2.0.8RC2/bin/"
-    train_data="../Data/Features/features.fb"
-    mallet_data="../Data/vectors/train.mallet"
+    root_dir=os.path.realpath(os.path.dirname(os.path.realpath("__file__"))+"/../")
+    print root_dir
+    mallet_bin=root_dir+"/mallet-2.0.8RC2/bin/"
+    train_data=root_dir+"/Data/Features/features.fb"
+    mallet_data=root_dir+"/Data/vectors/train.mallet"
 
 #Import Data
     print "Transforming data into vector..."
